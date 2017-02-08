8f1698b24f45d3196c4522555c9b735dd834dbbb
MitchHickox <mitchhickox@gmail.com>
2017-02-07 20:27:02 -0500

    Server Now Prints Database
    Server prints database automatically for now, will be changed to need a
    query first. Last refresh also prints.
    NOTE: an entry is added each time the page is loaded for now, just
    testing data entry.

--- public_html/index.php
+++ public_html/index.php
@@ -26,7 +26,7 @@
      INSERT INTO fsa
          (userName, email, password, type, fisName, time, description)
      VALUES
-         ('Test Name', 'Test@email','TestPassword', 'S', 'TestTypeName', NOW(), 'TEST Stomach Pain');
+         ('TestName', 'Test@email','TestPassword', 'S', 'TestTypeName', NOW(), 'TESTStomachPain');
 -->
 
 
@@ -42,6 +42,10 @@ try {
     $username = 'hickoxm';
     $password = 'password8989';
     $db = new PDO($dsn, $username, $password);
+
+
+
+    echo "Last Refreshed: " . date("Y/m/d") . "      " . date("h:i:sa") . "<br>";
 } catch (Exception $e) {
     echo "<h1>Error connecting to database.</h1>";
     echo "<p>Error : $e</p>";
@@ -58,6 +62,69 @@ try {
     This test page is intended for testing and debugging of the database and application, the
     FSA application will only post to this server (i.e. all html on this
     page is for testing only and will not be used by the app).</p>
+
+<?php
+
+try {
+
+
+    $db->exec("INSERT INTO fsa
+         (userName, email, password, type, fisName, time, description)
+     VALUES
+         ('TestName', 'Test@email','TestPassword', 'S', 'TestTypeName', NOW(), 'TESTStomachPain')");
+
+    /*
+    $stmt = $db->prepare("INSERT INTO fsa  VALUES ( :userName, :email, :password, :type, :fisName, :time, :desc)");
+
+    $uName = "TESTusername";
+    $email ="TESTemail";
+    $passwd ="TESTpassword";
+    $type = "S";
+    $fisName = "Headache";
+    $time = "NOW()";
+    $desc ="TEST head hurts=(";
+
+
+    $stmt->bindParam(' :userName', $uName);
+    $stmt->bindParam(' :email',$email);
+    $stmt->bindParam(' :password',$passwd);
+    $stmt->bindParam(' :type',$type);
+    $stmt->bindParam(' :fisName',$fisName);
+    $stmt->bindParam(' :time',$time);
+    $stmt->bindParam(' :desc',$desc);
+
+    $stmt->execute();
+    */
+
+
+
+    echo "<h1>Table</h1>";
+    $all = $db->query('SELECT * FROM fsa');
+    $count = 0;
+    while ($records = $all->fetchAll(PDO::FETCH_ASSOC)) {
+        ;
+
+        foreach($records as $record)
+        {
+            echo "<br><br>Row $count:<br>";
+            foreach ($record as $field)
+            {
+                echo "$field";
+                echo "||||";
+            }
+            $count += 1;
+        }
+        //print_r($record);
+
+        echo "<br>";
+    }
+} catch (Exception $e) {
+    echo "<h1>Error querying database.</h1>";
+    echo "<p>Error : $e</p>";
+    echo "<br><br><br>";
+}
+?>
+
 <h2>Enter Database Item</h2>
 <div>
 

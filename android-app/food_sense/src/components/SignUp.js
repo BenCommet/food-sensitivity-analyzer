0a27ca655c35b94a105d1e4eca23a3f7274123b2
androidDev <androiddev@host-57-74.mial48w.allendale.mi.us.clients.pavlovmedia.com>
2017-02-11 15:47:39 -0500

    Deleted default themes to allow for custom theming

--- android-app/food_sense/src/components/SignUp.js
+++ android-app/food_sense/src/components/SignUp.js
@@ -1,26 +1,34 @@
 import React, { Component} from 'react';
 import { StyleSheet } from 'react-native';
-import {Container, Header, Content, Footer, Title, Text, Button, Icon, View} from 'native-base';
+import {Container, Header, Title, Button, Subtitle, Left, Right, Body, Icon, StyleProvider } from 'native-base';
 import Dimensions from 'Dimensions';
 import headerTheme from '../Themes/headerTheme'
+import getTheme from '../native-base-theme';
+import material from '../native-base-theme/material';
 const height = Dimensions.get('window').height;
 const width = Dimensions.get('window').height;
 export default class SignUp extends Component{
 	render() {
 		return (
-			<View theme={headerTheme}>
+			<StyleProvider style={getTheme(material)}
 			<Container >
-				<Header >
-					<Button transparent>
-						<Icon name="menu" />
-					</Button>
-					<Title>Food Sense</Title>
-					<Button transparent>
-						<Icon name="settings" />
-					</Button>
+				<Header style = {{backgroundColor: '#029c88'}}>
+					<Left>
+						<Button transparent>
+							<Icon name="menu" />
+						</Button>
+					</Left>
+					<Body>
+						<Title>Food Sense</Title>
+					</Body>
+					<Right>
+						<Button transparent>
+							<Icon name="settings" />
+						</Button>
+					</Right>
 				</Header>
 			</Container>
-			</View>
+			</StyleProvider>
 		);
 	}
 } 

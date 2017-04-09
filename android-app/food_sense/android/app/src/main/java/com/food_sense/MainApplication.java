114242b020d60b73f77c2f4950831a969ed2088f
BenCommet <bencommet@gmail.com>
2017-04-09 16:27:24 -0400

    Added support for SVG

--- android-app/food_sense/android/app/src/main/java/com/food_sense/MainApplication.java
+++ android-app/food_sense/android/app/src/main/java/com/food_sense/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.horcrux.svg.SvgPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new SvgPackage()
       );
     }
   };
--- android-app/food_sense/src/components/AnalyzeSymptom.js
+++ android-app/food_sense/src/components/AnalyzeSymptom.js
@@ -2,6 +2,23 @@
 import React, { Component} from 'react';
 import { ScrollView, Modal, TouchableHighlight, Picker,
 	TextInput, Button, Alert} from 'react-native';
+import Svg,{
+    Circle,
+    Ellipse,
+    G,
+    LinearGradient,
+    RadialGradient,
+    Line,
+    Path,
+    Polygon,
+    Polyline,
+    Rect,
+    Symbol,
+    Text,
+    Use,
+    Defs,
+    Stop
+} from 'react-native-svg';
 
 
 export default class AnalyzeSymptom extends Component{
@@ -23,6 +40,28 @@ export default class AnalyzeSymptom extends Component{
 				  <Picker.Item label="8 Hours" value={8} />
 				  <Picker.Item label="24 Hours" value={24} />
 				</Picker>
+				<Svg
+                height="100"
+                width="100"
+            >
+                <Circle
+                    cx="50"
+                    cy="50"
+                    r="45"
+                    stroke="blue"
+                    strokeWidth="2.5"
+                    fill="green"
+                />
+                <Rect
+                    x="15"
+                    y="15"
+                    width="70"
+                    height="70"
+                    stroke="red"
+                    strokeWidth="2"
+                    fill="yellow"
+                />
+            </Svg>
 			</ScrollView>
 		)
 	}
